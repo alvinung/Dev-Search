38bc504b89d93b4afe5fe98be3c4efede7ca3191
Alvin Ung <alvin.ung@live.com>
2022-09-21 00:56:28 -0700

    added some test urls and views on projects app

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def projects(request):
+    return HttpResponse('Here are our projects!')
+
+
+def project(request, primary_key):
+    return HttpResponse('Single Project' + ' ' + str(primary_key))
