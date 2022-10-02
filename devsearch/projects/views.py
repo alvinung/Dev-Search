e72a3cda14bc57e90c2b5f5dcd2cc31652889723
Alvin Ung <alvin.ung@live.com>
2022-10-02 15:22:47 -0700

    created templates

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def projects(request):
-    return HttpResponse('Here are our projects!')
+    return render(request, 'projects/projects.html')
 
 
 def project(request, primary_key):
-    return HttpResponse('Single Project' + ' ' + str(primary_key))
+    return render(request, 'projects/single-project.html')
